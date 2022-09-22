67932738a746580d38468d6ef762d95c9e4be445
venkatp4 <79449355+venkatp4@users.noreply.github.com>
2022-09-22 16:34:14 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# XCFramworkAccessingViaPackageManager
+
+A description of this package.
---
+++ Sources/SwiftXCFramework.xcframework/ios-arm64/SwiftXCFramework.framework/Headers/SwiftXCFramework.h
@@ -0,0 +1,18 @@
+//
+//  SwiftXCFramework.h
+//  SwiftXCFramework
+//
+//  Created by Venkat on 22/09/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftXCFramework.
+FOUNDATION_EXPORT double SwiftXCFrameworkVersionNumber;
+
+//! Project version string for SwiftXCFramework.
+FOUNDATION_EXPORT const unsigned char SwiftXCFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftXCFramework/PublicHeader.h>
+
+
